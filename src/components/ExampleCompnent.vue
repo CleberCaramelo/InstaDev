c33bbc56b89d97de99439e0882329fe0b12efb49
CLEBER MARQUES DE JESUS <cleberjesus@pge.sc.gov.br>
2025-01-27 18:19:56 -0300

    Clear all project and add configs to estlint in .vscode file

---
+++ src/components/ExampleCompnent.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>example</h1>
+</template>
+
+<script>
+export default {
+  name: "ExampleComponent",
+  props: {
+    title: { type: String, required: true },
+  },
+};
+</script>
